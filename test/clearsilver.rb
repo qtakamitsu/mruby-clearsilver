f3ac4a1dfbc4009e9dbf54f0af8eb5a9f1d158fa
qtakamitsu <qtakamitsu@gmail.com>
2013-12-13 00:36:55 +0900

    add testcase(cs#render)

--- test/clearsilver.rb
+++ test/clearsilver.rb
@@ -4,3 +4,12 @@ assert("HDF#set, HDF#get") do
   hdf.get_value("foo", "") == "bar"
 end
 
+assert("CS#parse_string") do
+  hdf = ClearSilver::HDF.new
+  hdf.set_value "name", "Administrator"
+
+  cs = ClearSilver::CS.new(hdf)
+  cs.parse_string('Welcom to ClearSilver, <?cs var:name ?>!')
+  cs.render() == "Welcom to ClearSilver, Administrator!"
+end
+
